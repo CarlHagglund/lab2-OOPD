f4af898a970afb6667033f058c17dc309a719933
Alex Niko Nikic <alexnikic@outlook.com>
2026-02-16 14:58:44 +0100

    CarController startar och verkar fungera :)

--- CarView.java
+++ CarView.java
@@ -1,3 +1,5 @@
+package my.lab.cars;
+
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
--- DrawPanel.java
+++ DrawPanel.java
@@ -1,3 +1,5 @@
+package my.lab.cars;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
